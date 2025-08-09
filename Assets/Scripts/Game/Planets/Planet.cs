e133cce50edd366567c7756e46f0b0d9427109cf
Michael Xi <ximichael26@gmail.com>
2025-08-09 14:55:12 -0700

    fixed Planet and creature class spelling errors

--- Assets/Scripts/Game/Planets/Planet.cs
+++ Assets/Scripts/Game/Planets/Planet.cs
@@ -1,20 +1,22 @@
-[System.Serializable]
+using System;
+
+[Serializable]
 public class Planet
 {
     public int id;
     public string name;
     public string description;
     public PlanetConditions conditions;
-    public float gravity;
-    public float distanceFromEarth;
+    public float size;
 }
 
-[System.Serializable]
+[Serializable]
 public class PlanetConditions
 {
-    public float atmospherePressure;
-    public float radiationLevel;
-    public float averageTemperature;
-    public float oxygenLevel;
-    public float waterLevel;
+    public string planetType;       // "gas"
+    public float temperature;       // 1000
+    public float lengthOfYear;      // 100
+    public int habitableZone;       // 0 (e.g., index/flag)
+    public string atmosphere;       // "carbonDioxide"
+    public float distanceFromSun;   // 100
 }
--- Assets/Scripts/Game/RatingSystem.cs
+++ Assets/Scripts/Game/RatingSystem.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class RatingSystem
 {
-    public static float rating(Creature creature, Planet planet)
+    public static float GetCreaturePlanetRating(Creature creature, Planet planet)
     {
         //proposed: return a value from 1 - 10 based on creature traits, planet conditions matches
 
